17ff13311d367a51d6dfa639741701e23c4cb3d7
VIKRANT SHARMA <vikrant500sharma@gmail.com>
2023-07-11 14:36:06 +0530

    commit

--- 02-nodejs/authenticationServer.js
+++ 02-nodejs/authenticationServer.js
@@ -33,5 +33,61 @@ const express = require("express")
 const PORT = 3000;
 const app = express();
 // write your logic here, DONT WRITE app.listen(3000) when you're running tests, the tests will automatically start the server
+app.use(bodyParser.json());
 
+const users=[];
+
+
+
+app.post('/signup', (req, res) => {
+  const { username, password, firstName, lastName } = req.body;
+
+
+  const existingUser = users.find(user => user.username === username);
+  if (existingUser) {
+    return res.status(400).json({ error: 'Username already exists' });
+  }
+
+  const id=Date.now().toString();
+
+  const newUser={id,username,password,firstName,lastName};
+  users.push(newUser);
+
+  return res.status(201).json({message:'User created successfully'});
+});
+
+app.post("/login", (req, res) => {
+ const {username,password}=req.body;
+
+ const user=users.find(user=>user.username===username);
+
+
+ if(user && user.password===password){
+  const {id,firstName,lastName}=user;
+  return res.status(200).json({id,firstName,lastName});
+
+ }
+
+ return res.status(401).json({error:'Invalid Credentials'});
+});
+
+app.get('/data',(req,res)=>{
+
+  const {username,password}=req.headers;
+
+
+  const user=users.find(user=>user.username==username && user.password===password);
+
+  if(!user){
+    return res.status(401).json({errir:'Invalid Credentials'});
+  }
+
+  const userDetails = users.map(({ id, firstName, lastName }) => ({ id, firstName, lastName }));
+  return res.status(200).json({ users: userDetails });
+});
+
+app.use((req,res)=>{
+  res.status(404).json({error:'Not Found'});
+
+});
 module.exports = app;
